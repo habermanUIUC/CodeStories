a8bf9f724a5c267b8749a3a30dd0443f1463f526
mike haberman <mikeh.hasmail@gmail.com>
2021-08-11 12:11:43 -0700

    first commit

---
+++ lessons/sp21/p4ds/cc/plotting_potter/lib/NotebookUtil.py
@@ -0,0 +1,91 @@
+
+import requests
+import urllib.parse
+import urllib.request
+import os
+
+def is_ipython(text):
+    return text is not None and text.find('{"nbformat') == 0
+
+def validate_notebook_id(notebook_id):
+    import re
+    regex = re.compile(r'/?[A-Za-z0-9_-]{20,}')
+    ids = regex.findall(notebook_id)
+    if len(ids) == 0:
+        return None
+    return ids[0].strip('/')
+
+def install_gd_file(doc_id, filename=None):
+
+    #
+    # possible 403 if attempt is made too many times to download?
+    # seems to be temporary -- don't fire off too many requests
+    #
+    baseurl = "https://docs.google.com/uc"
+    baseurl = "https://drive.google.com/uc"
+
+    #
+    # can help by switching the baseurl
+    #
+
+    params = {"export": "download", "id": doc_id}
+    url = baseurl + "?" + urllib.parse.urlencode(params)
+
+    try:
+
+        def v2():
+            #r = requests.get(baseurl, params)
+            r = requests.get(url)
+            r.encoding = 'utf-8'
+            if r.status_code != 200:
+                print('bad request:', r.status_code)
+                print('headers:', r.headers)
+                print(r.status_code, "unable to download google doc with id:", doc_id)
+                return None
+            return r.text
+
+        text = v2()
+        if filename is not None and text is not None and len(text) > 0:
+            with open(filename, 'w') as fd:
+                fd.write(text)
+        else:
+            print("Unable to write file", filename, len(text))
+
+        return text
+
+    except Exception as e:
+        print("unable to load notebook at", url, str(e))
+        return None
+
+def mount_notebook(doc_url, silent=True, idx=None):
+    import CodeCleaners
+    import Notebook
+    import Extractor
+
+    doc_id = validate_notebook_id(doc_url)
+    text = install_gd_file(doc_id, 'nb.ipynb')
+
+    #text = open(name, 'r').read()
+
+    cleaner = CodeCleaners.CodeCleaner()
+    story = Notebook.StoryNotebook(text)
+
+    code = cleaner.clean(story)
+    filename = 'lesson.py'
+    module = 'lesson'
+    if idx is not None:
+        filename = "lesson{:d}.py".format(idx)
+        module = "lesson{:d}".format(idx)
+
+    Extractor.create_module(code=code, file_out=filename, hide_imports=False)
+
+    try:
+        import importlib
+        lesson = importlib.import_module(module)
+        #import os
+        #os.remove(filename)
+        return True
+    except Exception as e:
+        print('Exception ', e)
+        return False
+
---
+++ resources/pre_commit.sh
@@ -0,0 +1,47 @@
+#!/usr/local/bin/zsh
+#!/bin/bash
+# get all the files that are ready to commit
+# user has executed git add . 
+
+root="/Users/mikeh/home/projects/CodeStoryLessons"
+root="/Users/mikeh/home/projects/codeStories-lessons"
+
+if [ $# -eq 0 ]; then
+  out=$(git diff-index --cached Head | cut -f2 | grep lessons | cut -f1-4 -d/ | uniq)
+else
+  echo 'process params'
+  out="$1"
+  # convert tag dmap:projects:project-m3 into lessons/dmap/projects/project-m3
+fi
+
+IFS=' '
+while read -r line;do
+  echo "Processing ${line}"
+  SRC_DIR=$root/$line
+  dir=$(echo ${line}| cut -f1 -d/)
+  echo "Testing ${dir}"
+
+  if [ ${dir} = 'lessons' ]; then
+    LESSON=$(echo ${line}| cut -f4 -d/)
+    echo "doing ${TODO} ${line} ${dir} ${LESSON}"
+    #echo "${SRC_DIR}"
+    # cd to the directory so tar only keeps relative path
+    #     --exclude='*.ipynb' --exclude='*.gz' ${line} &> /tmp/log.txt
+    # NOTE: some lessons have .gz datasets
+    cd "${root}"
+    gtar --sort=name --owner=root:0 --group=root:0 --mtime='UTC 2021-01-01' \
+         -cvf ${LESSON}.tar \
+         --exclude='*.ipynb' --exclude='*.tar.gz' ${line} &> /tmp/log.txt
+    gzip -n ${LESSON}.tar
+    /bin/mv ${LESSON}.tar.gz ${SRC_DIR}
+    ls -la ${SRC_DIR}/*.gz
+    git add ${SRC_DIR}
+    echo 'tar file added via git'
+    echo 'be sure to git commit'
+
+  else
+    echo "skip ${line}"
+  fi
+
+done <<< "$out"
+
